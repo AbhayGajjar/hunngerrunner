7a5b6faddce1240db3c5b97ec177399fff1cb2ef
AbhayGajjar <abhaymgajjar@gmail.com>
2024-03-11 20:22:45 +0530

    remove in package.json

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,15 @@
 import RestaurantHeader from '@/components/RestaurantHeader'
-import React from 'react'
+import React from 'react';
+import { FaPeopleGroup } from "react-icons/fa6";
 
 const Home = () => {
   return (
     <>
       <RestaurantHeader />
-    <div>Home</div>
+      <div className=" tooltip tooltip-right" data-tip="customer">
+        <FaPeopleGroup className='hover:cursor-pointer text-2xl'/>
+          
+      </div>
     </>
   )
 }
